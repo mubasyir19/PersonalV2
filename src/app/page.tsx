7c57e27ba89dfe0ab38884297f555402da26d3cd
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-14 16:40:11 +0700

    add(about): about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from '@/components/About';
 import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main>
+    <main className='px-3 py-2'>
       <Hero />
+      <About />
     </main>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <p>About</p>
-    </div>
+    <section className='text-white text-center'>
+      <h1 className='text-2xl lg:text-4xl'>About</h1>
+      <p className='text-xl mt-12 mb-4'>
+        I am an informatics engineering student at Gunadarma University. Interested in web development, both frontend
+        and backend.
+      </p>
+    </section>
   );
 }
