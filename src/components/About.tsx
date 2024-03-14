b86129cbd3e6cf0e0aac1eea2144c20559abda53
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-14 16:43:33 +0700

    fix(about): fix padding

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <section className='text-white text-center'>
+    <section className='px-3 text-white text-center'>
       <h1 className='text-2xl lg:text-4xl'>About</h1>
       <p className='text-xl mt-12 mb-4'>
         I am an informatics engineering student at Gunadarma University. Interested in web development, both frontend
