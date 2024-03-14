4e3e10ce27e8e2a1d4a873d97b526fe0c10c9db8
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-14 16:42:36 +0700

    fix(hero): fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <main className='px-3 py-2'>
+    <main>
       <Hero />
       <About />
     </main>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <section className='px-20 mb-20'>
+    <section className='px-12 mb-20'>
       <div className='py-20 w-full'>
         <Image
           src='/images/me.jpeg'
