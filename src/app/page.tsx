ae8d21a5ae524474c125a3c06bfa05795a422030
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-14 17:01:51 +0700

    add(skill): skill section layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from '@/components/About';
 import Hero from '@/components/Hero';
+import Skill from '@/components/Skill';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Skill />
     </main>
   );
 }
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Skill() {
   return (
-    <div>
-      <p>Skill</p>
-    </div>
+    <section className='px-3 lg:px-28 mt-12 text-white'>
+      <h1 className='text-center mb-2 text-2xl lg:text-4xl'>My Skills</h1>
+      <p className='text-xl my-4'>Programming Language</p>
+      <p className='text-xl my-4'>Library / Framework</p>
+      <p className='text-xl my-4'>Database</p>
+      <p className='text-xl my-4'>Software / Tools</p>
+    </section>
   );
 }
