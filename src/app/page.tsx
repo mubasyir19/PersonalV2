3d06dcee2f40c35c69c9bcb292babc12253ec3d9
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-15 14:12:16 +0700

    update(skills): add scroll style AOS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
 import About from '@/components/About';
 import Hero from '@/components/Hero';
 import Skill from '@/components/Skill';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <main>
       <Hero />
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,94 +1,169 @@
 import Image from 'next/image';
-import React from 'react';
 
 export default function Skill() {
   return (
     <section className='px-3 lg:px-28 mt-12 text-white'>
       <h1 className='text-center mb-2 text-2xl lg:text-4xl'>My Skills</h1>
       <p className='text-xl my-4'>Programming Language</p>
-      <div className='grid grid-cols-1 gap-4 lg:grid-cols-4'>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-4'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/HTML.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='html' />
           <p className='mt-4 text-xl font-semibold'>HTML</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/CSS.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='css' />
           <p className='mt-4 text-xl font-semibold'>CSS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/javascript.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='javascript' />
           <p className='mt-4 text-xl font-semibold'>JavaScript</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/golang.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='golang' />
           <p className='mt-4 text-xl font-semibold'>Golang</p>
         </div>
       </div>
       <p className='text-xl my-4'>Library / Framework</p>
-      <div className='grid grid-cols-1 gap-4 lg:grid-cols-4'>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-4'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/reactjs.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='reactjs' />
           <p className='mt-4 text-xl font-semibold'>React JS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/node.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='nodejs' />
           <p className='mt-4 text-xl font-semibold'>Node JS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/next.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='nextjs' />
           <p className='mt-4 text-xl font-semibold'>Next JS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/express.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='expressjs' />
           <p className='mt-4 text-xl font-semibold'>Express JS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/reactjs.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='react-native' />
           <p className='mt-4 text-xl font-semibold'>React Native</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/tailwind.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='tailwindcss' />
           <p className='mt-4 text-xl font-semibold'>TailwindCSS</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/bootstrap.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='bootstrap' />
           <p className='mt-4 text-xl font-semibold'>Bootstrap</p>
         </div>
       </div>
       <p className='text-xl my-4'>Database</p>
-      <div className='grid grid-cols-1 gap-4 lg:grid-cols-4'>
-        <div className='card w-full py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-4'>
+        <div
+          className='card w-full py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/mongodb.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='mongodb' />
           <p className='mt-4 text-xl font-semibold'>MongoDB</p>
         </div>
-        <div className='card w-full py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card w-full py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/mysql.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='mysql' />
           <p className='mt-4 text-xl font-semibold'>MySQL</p>
         </div>
       </div>
       <p className='text-xl my-4'>Software / Tools</p>
-      <div className='grid grid-cols-1 gap-4 lg:grid-cols-4'>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-4'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/vscode.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='vscode' />
           <p className='mt-4 text-xl font-semibold'>VsCode</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/git.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='git' />
           <p className='mt-4 text-xl font-semibold'>Git</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/github-white.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='github' />
           <p className='mt-4 text-xl font-semibold'>Github</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/postman.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='postman' />
           <p className='mt-4 text-xl font-semibold'>Postman</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/figma.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='figma' />
           <p className='mt-4 text-xl font-semibold'>Figma</p>
         </div>
-        <div className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'>
+        <div
+          className='card py-8 text-center border-2 border-[#002352] rounded-md bg-gradient-to-tr from-blue-900 via-black to-black'
+          data-aos='flip-left'
+          data-aos-duration='1000'
+        >
           <Image src='/icons/gcloud.png' height={72} width={72} className='mx-auto h-20 w-auto' alt='gcloud' />
           <p className='mt-4 text-xl font-semibold'>Google Cloud</p>
         </div>
