f8cfe1b7cb472e17b71fafabd1cf2a0aa054ce69
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-05-07 19:52:01 +0700

    update(api, skill): error response get data skill

--- src/app/api/skill/route.ts
+++ src/app/api/skill/route.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { error } from 'console';
 import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
@@ -8,7 +7,7 @@ export const GET = async (req: NextRequest) => {
   const dataSkills = await prisma.skill.findMany();
 
   if (!dataSkills) {
-    return NextResponse.json({ message: 'an error occured', error }, { status: 500 });
+    return NextResponse.json({ message: 'an error occured' }, { status: 500 });
   }
 
   return NextResponse.json({ message: 'success get all data', data: dataSkills });
