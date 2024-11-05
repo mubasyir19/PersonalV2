7f362d0836145993dc51aadf0d7298fc0c5bbe9f
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-11-05 11:51:36 +0700

    delete: delete api

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,82 +1,33 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-
 export default function Login() {
-  const router = useRouter();
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      const data = await response.json();
-
-      // console.log('response', data.token);
-      // console.log('response data', data);
-
-      if (data.token) {
-        Swal.fire({
-          title: 'Sign in successfully',
-          icon: 'success',
-        });
-
-        console.log('TOKEN => ', data.token);
-
-        const tokenBase64 = btoa(data.token);
-        localStorage.setItem('token', tokenBase64);
-        router.push('/dashboard');
-      } else {
-        Swal.fire({
-          title: 'Failed Sign in ',
-          icon: 'error',
-          text: data.message,
-        });
-      }
-    } catch (error) {
-      console.error('An error occured', error);
-    }
-  };
-
   return (
     <form
-      className='w-80 p-4 flex flex-col gap-y-4 bg-gradient-to-tl border border-blue-900 from-blue-900 to-black rounded-lg'
-      onSubmit={handleSubmit}
+      className="flex w-80 flex-col gap-y-4 rounded-lg border border-blue-900 bg-gradient-to-tl from-blue-900 to-black p-4"
+      onSubmit={() => alert("under maintainance")}
     >
-      <h2 className='text-2xl font-semibold text-center text-white'>Login</h2>
-      <div className='form-group'>
-        <label className='text-white'>Username</label>
+      <h2 className="text-center text-2xl font-semibold text-white">Login</h2>
+      <div className="form-group">
+        <label className="text-white">Username</label>
         <input
-          type='text'
-          name='username'
-          className='w-full p-2 border-2 text-base text-white border-slate-300 rounded-lg placeholder:italic placeholder:text-slate-400 placeholder:text-sm bg-transparent'
-          placeholder='Type your username'
-          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          name="username"
+          className="w-full rounded-lg border-2 border-slate-300 bg-transparent p-2 text-base text-white placeholder:text-sm placeholder:italic placeholder:text-slate-400"
+          placeholder="Type your username"
         />
       </div>
-      <div className='form-group'>
-        <label className='text-white'>Password</label>
+      <div className="form-group">
+        <label className="text-white">Password</label>
         <input
-          type='password'
-          name='password'
-          className='w-full p-2 border-2 text-base text-white border-slate-300 rounded-lg placeholder:italic placeholder:text-slate-400 placeholder:text-sm bg-transparent'
-          placeholder='Type your password'
-          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          name="password"
+          className="w-full rounded-lg border-2 border-slate-300 bg-transparent p-2 text-base text-white placeholder:text-sm placeholder:italic placeholder:text-slate-400"
+          placeholder="Type your password"
         />
       </div>
-      <div className='form-group'>
-        <button type='submit' className='w-full py-2 text-center text-white bg-blue-500 hover:bg-blue-600 rounded-lg'>
+      <div className="form-group">
+        <button
+          type="submit"
+          className="w-full rounded-lg bg-blue-500 py-2 text-center text-white hover:bg-blue-600"
+        >
           Login
         </button>
       </div>
--- src/app/(dashboard)/dashboard/skills/page.tsx
+++ src/app/(dashboard)/dashboard/skills/page.tsx
@@ -1,110 +1,44 @@
-'use client';
-
-import { PlusIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-
-interface SkillType {
-  name: string;
-}
+import { PlusIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function Skills() {
-  const [skills, setSkills] = useState<SkillType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const dataSkills = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch('/api/skill');
-        const data = await response.json();
-        console.log(data);
-        setSkills(data.data);
-      } catch (error) {
-        console.log('Error occured', error);
-        setError('An error occurred while fetching data');
-      }
-      setIsLoading(false);
-    };
-
-    dataSkills();
-  }, []);
-
   return (
     <>
-      <div className='flex justify-between mb-5'>
-        <h2 className='my-auto text-white text-2xl'>Skills</h2>
-        <div className='my-auto text-white text-sm flex gap-x-1'>
-          <Link href='/dashboard' className='text-sky-400'>
+      <div className="mb-5 flex justify-between">
+        <h2 className="my-auto text-2xl text-white">Skills</h2>
+        <div className="my-auto flex gap-x-1 text-sm text-white">
+          <Link href="/dashboard" className="text-sky-400">
             Dashboard
           </Link>
-          /<p className='text-white'>Skills</p>
+          /<p className="text-white">Skills</p>
         </div>
       </div>
-      <div className='mt-10'>
-        <div className='mb-8 text-white'>
+      <div className="mt-10">
+        <div className="mb-8 text-white">
           <Link
-            href='#skills/tambah'
-            className='w-fit text-sm p-2 flex gap-x-1 bg-sky-400 hover:bg-sky-500 transition-all rounded-lg'
+            href="#skills/tambah"
+            className="flex w-fit gap-x-1 rounded-lg bg-sky-400 p-2 text-sm transition-all hover:bg-sky-500"
           >
-            <PlusIcon className='h-6 w-6' />
-            <span className='my-auto'>Tambah</span>
+            <PlusIcon className="h-6 w-6" />
+            <span className="my-auto">Tambah</span>
           </Link>
         </div>
-        <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
-          <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
-            <thead className='text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
+            <thead className="bg-gray-50 text-sm uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   No
                 </th>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   Skill Name
                 </th>
-                <th scope='col' className='px-6 py-3'>
+                <th scope="col" className="px-6 py-3">
                   Action
                 </th>
               </tr>
             </thead>
-            <tbody>
-              {isLoading ? (
-                <>
-                  <tr className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
-                    <td className='px-6 py-4 font-medium text-gray-900 dark:text-white text-center' colSpan={3}>
-                      Loading...
-                    </td>
-                  </tr>
-                </>
-              ) : error ? (
-                <>
-                  <tr className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
-                    <td className='px-6 py-4 font-medium text-gray-900 dark:text-white text-center' colSpan={3}>
-                      An error occured failed get data ...
-                    </td>
-                  </tr>
-                </>
-              ) : (
-                <>
-                  {skills.map((skill, index) => (
-                    <>
-                      <tr className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
-                        <td className='px-6 py-4 font-medium text-gray-900 dark:text-white'>{index + 1}</td>
-                        <td className='px-6 py-4'>{skill.name}</td>
-                        <td className='px-6 py-4 flex gap-x-4'>
-                          <a href='#' className='font-medium text-blue-600 dark:text-blue-500 hover:underline'>
-                            Edit
-                          </a>
-                          <a href='#' className='font-medium text-red-600 dark:text-red-500 hover:underline'>
-                            hapus
-                          </a>
-                        </td>
-                      </tr>
-                    </>
-                  ))}
-                </>
-              )}
-            </tbody>
+            <tbody></tbody>
           </table>
         </div>
       </div>
